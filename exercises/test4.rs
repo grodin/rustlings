2913eb8ceea971963589840b2a199d77890c8e01
Joseph Cooper <cooper.joseph@gmail.com>
2020-06-05 20:05:56 +0100

    Solved everything up to Arc exercises

--- exercises/test4.rs
+++ exercises/test4.rs
@@ -5,7 +5,11 @@
 
 // Write a macro that passes the test! No hints this time, you can do it!
 
-// I AM NOT DONE
+macro_rules! my_macro {
+    ($var:literal) => {
+        concat!("Hello ",$var)
+    };
+}
 
 #[cfg(test)]
 mod tests {
